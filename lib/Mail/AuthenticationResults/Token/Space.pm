f0b394df10f5fe8c27c9f135c5f89823b05d9006
Marc Bradshaw <marc@marcbradshaw.net>
2018-03-27 13:25:43 +1100

    Added the Space token class

---
+++ lib/Mail/AuthenticationResults/Token/Space.pm
@@ -0,0 +1,29 @@
+package Mail::AuthenticationResults::Token::Space;
+# ABSTRACT: Class for modelling AuthenticationResults Header parts detected as spaces
+
+require 5.008;
+use strict;
+use warnings;
+# VERSION
+use Carp;
+
+use base 'Mail::AuthenticationResults::Token';
+
+=head1 DESCRIPTION
+
+Token representing a space
+
+=cut
+
+sub is {
+    my ( $self ) = @_;
+    return 'space';
+}
+
+sub parse {
+    my ($self) = @_;
+    croak 'Space tokens are not used in parsing';
+}
+
+1;
+
